f94071d06f28e009b508e561f049a93d0bf72c6c
luxreduxdelux <juanignacioiglesias00@gmail.com>
2025-12-04 19:51:53 -0300

    add base-line API

---
+++ engine/src/module/font.rs
@@ -0,0 +1,143 @@
+/*
+* Copyright (c) 2025 luxreduxdelux
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* Subject to the terms and conditions of this license, each copyright holder
+* and contributor hereby grants to those receiving rights under this license
+* a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+* (except for failure to satisfy the conditions of this license) patent license
+* to make, have made, use, offer to sell, sell, import, and otherwise transfer
+* this software, where such license applies only to those patent claims, already
+* acquired or hereafter acquired, licensable by such copyright holder or
+* contributor that are necessarily infringed by:
+*
+* (a) their Contribution(s) (the licensed copyrights of copyright holders and
+* non-copyrightable additions of contributors, in source or binary form) alone;
+* or
+*
+* (b) combination of their Contribution(s) with the work of authorship to which
+* such Contribution(s) was added by such copyright holder or contributor, if,
+* at the time the Contribution is added, such addition causes such combination
+* to be necessarily infringed. The patent license shall not apply to any other
+* combinations which include the Contribution.
+*
+* Except as expressly stated above, no rights or licenses from any copyright
+* holder or contributor is granted under this license, whether expressly, by
+* implication, estoppel or otherwise.
+*
+* DISCLAIMER
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+use crate::module::general::*;
+use engine_macro::*;
+
+//================================================================
+
+use mlua::prelude::*;
+use raylib::prelude::*;
+
+//================================================================
+
+#[module(name = "font", info = "Font API.")]
+pub fn set_global(lua: &mlua::Lua, global: &mlua::Table) -> anyhow::Result<()> {
+    let font = lua.create_table()?;
+
+    font.set("new", lua.create_function(self::Font::new)?)?;
+
+    global.set("font", font)?;
+
+    Ok(())
+}
+
+#[class(info = "Font class.")]
+struct Font {
+    inner: ffi::Font,
+}
+
+impl Font {
+    #[function(
+        from = "font",
+        info = "Create a new font resource.",
+        parameter(name = "path", info = "Path to font.", kind = "string"),
+        result(name = "data", info = "File data.", kind = "user_data")
+    )]
+    fn new(_: &mlua::Lua, path: String) -> mlua::Result<Self> {
+        unsafe {
+            let inner = ffi::LoadFont(c_string(&path).as_ptr());
+
+            if ffi::IsFontValid(inner) {
+                Ok(Self { inner })
+            } else {
+                Err(mlua::Error::runtime(format!(
+                    "font.new(): Error loading font \"{path}\"."
+                )))
+            }
+        }
+    }
+
+    #[method(
+        from = "font",
+        info = "Draw text.",
+        parameter(name = "text", info = "Text to draw.", kind = "string"),
+        parameter(name = "point", info = "Point of text to draw.", kind = "vector_2"),
+        parameter(name = "scale", info = "Scale of text to draw.", kind = "number"),
+        parameter(name = "space", info = "Space of text to draw.", kind = "number"),
+        parameter(name = "color", info = "Color of text to draw.", kind = "color")
+    )]
+    fn draw(
+        lua: &mlua::Lua,
+        this: &Self,
+        (text, point, scale, space, color): (String, mlua::Value, f32, f32, mlua::Value),
+    ) -> mlua::Result<()> {
+        unsafe {
+            let point: Vector2 = lua.from_value(point)?;
+            let color: Color = lua.from_value(color)?;
+
+            ffi::DrawTextEx(
+                this.inner,
+                c_string(&text).as_ptr(),
+                point.into(),
+                scale,
+                space,
+                color.into(),
+            );
+            Ok(())
+        }
+    }
+}
+
+impl Drop for Font {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::UnloadFont(self.inner);
+        }
+    }
+}
+
+impl mlua::UserData for Font {
+    fn add_methods<M: mlua::UserDataMethods<Self>>(method: &mut M) {
+        method.add_method("draw", |lua, this, (text, point, scale, space, color)| {
+            Self::draw(lua, this, (text, point, scale, space, color))
+        });
+    }
+}
---
+++ engine/src/module/window.rs
@@ -0,0 +1,111 @@
+/*
+* Copyright (c) 2025 luxreduxdelux
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+* this list of conditions and the following disclaimer in the documentation
+* and/or other materials provided with the distribution.
+*
+* Subject to the terms and conditions of this license, each copyright holder
+* and contributor hereby grants to those receiving rights under this license
+* a perpetual, worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+* (except for failure to satisfy the conditions of this license) patent license
+* to make, have made, use, offer to sell, sell, import, and otherwise transfer
+* this software, where such license applies only to those patent claims, already
+* acquired or hereafter acquired, licensable by such copyright holder or
+* contributor that are necessarily infringed by:
+*
+* (a) their Contribution(s) (the licensed copyrights of copyright holders and
+* non-copyrightable additions of contributors, in source or binary form) alone;
+* or
+*
+* (b) combination of their Contribution(s) with the work of authorship to which
+* such Contribution(s) was added by such copyright holder or contributor, if,
+* at the time the Contribution is added, such addition causes such combination
+* to be necessarily infringed. The patent license shall not apply to any other
+* combinations which include the Contribution.
+*
+* Except as expressly stated above, no rights or licenses from any copyright
+* holder or contributor is granted under this license, whether expressly, by
+* implication, estoppel or otherwise.
+*
+* DISCLAIMER
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+use engine_macro::*;
+
+//================================================================
+
+use mlua::prelude::*;
+use raylib::prelude::*;
+
+//================================================================
+
+#[module(name = "window", info = "Window API.")]
+pub fn set_global(lua: &mlua::Lua, global: &mlua::Table) -> anyhow::Result<()> {
+    let window = lua.create_table()?;
+
+    window.set("get_exit", lua.create_function(self::get_exit)?)?;
+    window.set("wipe", lua.create_function(self::wipe)?)?;
+    window.set("draw", lua.create_function(self::draw)?)?;
+
+    global.set("window", window)?;
+
+    Ok(())
+}
+
+#[function(
+    from = "window",
+    info = "Get the exit state of the window.",
+    result(name = "state", info = "Exit state.", kind = "boolean")
+)]
+fn get_exit(_: &mlua::Lua, _: ()) -> mlua::Result<bool> {
+    unsafe { Ok(ffi::WindowShouldClose()) }
+}
+
+#[function(
+    from = "window",
+    info = "Wipe the frame-buffer.",
+    parameter(
+        name = "color",
+        info = "Color to wipe the frame-buffer with.",
+        kind = "color"
+    )
+)]
+fn wipe(lua: &mlua::Lua, color: mlua::Value) -> mlua::Result<()> {
+    unsafe {
+        let color: Color = lua.from_value(color)?;
+        ffi::ClearBackground(color.into());
+        Ok(())
+    }
+}
+
+#[function(
+    from = "window",
+    info = "Initialize a draw session.",
+    parameter(name = "call", info = "Draw function.", kind = "function")
+)]
+fn draw(_: &mlua::Lua, call: mlua::Function) -> mlua::Result<()> {
+    unsafe {
+        ffi::BeginDrawing();
+        call.call::<()>(())?;
+        ffi::EndDrawing();
+        Ok(())
+    }
+}
